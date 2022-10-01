4b06351352120a6effc002cdd7c08cc175966b8c
leethanhphat <106573278+leethanhphat@users.noreply.github.com>
2022-10-02 01:51:20 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -17,6 +17,8 @@ namespace touch
         const int TimeMedium = 150;
         const int TimeHard = 70;
 
+        static bool GameOver = false;
+
         static void KhoiTao()
         {
             Console.WriteLine("Chon muc do (De, Trung Binh, Kho)");
@@ -35,6 +37,20 @@ namespace touch
             for (int i = 1; i <= Length_Game; i++)
                 Console.Write('_');
         }
+
+        static void initLine()
+
+        {
+            Random rd = new Random();
+            int n = rd.Next(1, 5);
+            for (int i = 1; i <= n; i++)
+            {
+                string TextRd = Convert.ToString((char)rd.Next(97, 122));
+                int NumRd = rd.Next(0, Length_Game - 1);
+                if (Array[0, NumRd] == " ") Array[0, NumRd] = TextRd;
+            }
+        }
+
         static void RoiChu()
         {
             Random rd = new Random();
@@ -49,23 +65,16 @@ namespace touch
             t2.Start();
             do
             {
-                Thread t3 = new Thread(() =>
-                {
-
-                    int n = rd.Next(1, 5);
-                    for (int i = 1; i <= n; i++)
-                    {
-                        string TextRd = Convert.ToString((char)rd.Next(97, 122));
-                        int NumRd = rd.Next(0, Length_Game - 1);
-                        if (Array[0, NumRd] == " ") Array[0, NumRd] = TextRd;
-                    }
-                });
-                t3.Start();
+                initLine();
                 Push();
+                // Nếu GameOver thì thoát khỏi vòng lặp thôi
+                if (GameOver) break;
+                
             } while (true);
 
-
+            // Game Over. In ra cái gì đó ở đây hoặc ở hàm main
         }
+
         static void Write()
         {
             Console.Clear();
@@ -78,20 +87,51 @@ namespace touch
             TaoThanhDung();
             Thread.Sleep(Time);
         }
+
         static void Push()
         {
             for (int i = 0; i <= 29; i++)
             {
+                // Nếu GameOver thì thoát khỏi vòng lặp thôi
+                if (GameOver) return;
+
+
+                // Lấy ra key mới nhất, tránh trường hợp đang chạy mà st bị thay đổi
+                string latestKey = st;
+                latestKey = char.Parse(Console.ReadLine());
+                // Biến đánh dấu kí tự có xuất hiện không
+                bool keyInLine = false;
                 for (int j = 0; j <= 149; j++)
                 {
-                    if (Array[i, j] == st) Array[i, j] = " ";
+                    if (Array[i, j] == latestKey)
+                    {
+                        Array[i, j] = " ";
+                        keyInLine = true;
+                    }
                     else if (Array[i, j] != " " && i < 29)
                     {
                         Array[i + 1, j] = Array[i, j];
                         Array[i, j] = " ";
                     }
-                    else if (i == 29) Array[i, j] = " ";
+
+                    // Xử lí game over khi chạm vạch
+                    if (i == 29 && Array[i, j] != " ")
+                    {
+                        GameOver = true;
+                    }
                 }
+
+                // Xử lí game over. Game over xảy ra khi thỏa mãn 2 điều kiện:
+                // 1: latestKey != "" => user có nhấn nút
+                // 2: keyInLine == false => không tìm thấy kí tự
+                if (latestKey != "" && keyInLine == false)
+                {
+                    GameOver = true;    
+                }
+
+                // Nhớ reset biến st, không thì vòng lặp sau sẽ tạch do st vẫn giữ kí tự cũ
+                st = "";
+
                 Write();
             }
         }
@@ -100,6 +140,8 @@ namespace touch
             KhoiTao();
             TaoThanhDung();
             RoiChu();
+            
+            Console.WriteLine("\n\n GameOver");
         }
     }
 }
